a3c957f7f54c97f71ba9e1604259052da4a65b71
lj <928904929@qq.com>
2014-03-06 12:17:16 +0800

    db

--- models/db.js
+++ models/db.js
@@ -2,4 +2,4 @@ var settings = require('../settings');
 var Db = require('mongodb').Db;
 var Server = require('mongodb').Server;
 
-module.exports = new Db(settings.db_name,new Server(settings.db_host,settings.db_port,{}));
+module.exports = new Db(settings.db_name,new Server(settings.db_host,settings.db_port,{}),{safe:false});
