136d5f685379f9e2a5ca3bce48ce2ca885454299
lj <928904929@qq.com>
2014-03-06 12:02:25 +0800

    db

--- models/db.js
+++ models/db.js
@@ -1,4 +1,5 @@
 var settings = require('../settings');
 var Db = require('mongodb').Db;
+var Server = require('mongodb').Server;
 
 module.exports = new Db(settings.db_name,new Server(settings.db_host,settings.db_port,{}));
