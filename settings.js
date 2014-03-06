ccf2069680dc2ac0e981c04983d4192bbad66723
lj <928904929@qq.com>
2014-03-06 12:51:56 +0800

    db

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 module.exports = {
 	cookieSecret: 'icsser',
-	db_name: 'yourmongodbname',
+	db_name: 'EquuFIqxPbKLKpdhsSpT',
 	db_f: 'db_icsser',
 	db_host: 'mongo.duapp.com',
-	db_port: '27017',
+	db_port: '8908',
 	username: 'WDoXb1LUjfCZVTPg4mROIizn',
 	password: 'ZSAqBllra7n4LeCVjbpsGg0i0cyaDGdU'
 }
